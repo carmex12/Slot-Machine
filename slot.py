d7caf5b97b6356a0d5d515da2d4f8c67a3bfb9cf
Justin Elak <justin.elak@gmail.com>
2026-01-19 01:53:30 -0500

    added bet amount

--- slot.py
+++ slot.py
@@ -5,32 +5,33 @@ import time
 
 symbols = ["7","🍒","🍊","🍉","🍇"]
 
-scoreDict = {("7","7","7"):1000,
-    ("🍒","🍒","🍒"):750,
-    ("🍊","🍊","🍊"):500,
-    ("🍉","🍉","🍉"):250,
-    ("🍇","🍇","🍇"):100}
+scoreDict = {("7","7","7"):10,
+    ("🍒","🍒","🍒"):5,
+    ("🍊","🍊","🍊"):4,
+    ("🍉","🍉","🍉"):3,
+    ("🍇","🍇","🍇"):2}
 
 class slot:
-    def __init__(self,starting_balance):
+    def __init__(self,starting_balance ,bet = 10):
         self.balance = starting_balance
         self.cooldown = False
+        self.bet = bet
 
     def spin(self):
-        if not self.cooldown:
+        if not self.cooldown and self.balance>=self.bet:
             self.cooldown = True
             spinButton.config(state="disabled")
-            self.balance = self.balance - 50
+            self.balance -= self.bet
             result = []
             for _ in range(3):
                 result.append(random.choice(symbols))
             self.animation()
             output1.config(text=result[0])
             output2.config(text=result[1])
-            output3.config(text=result[2])
+            output3.config(text=result[2]) 
             result = tuple(result)
             if result in scoreDict:
-                self.balance += scoreDict[result]
+                self.balance += scoreDict[result]*self.bet
             score.config(text=f"Balance: {slot.balance}")
             time.sleep(0.1)
             spinButton.config(state="normal")
@@ -41,7 +42,15 @@ class slot:
             output2.config(text=random.choice(symbols))
             output3.config(text=random.choice(symbols))
             root.after(100, self.animation, i + 1)
-        
+    def increase_bet(self):
+        if self.bet < 100:
+            self.bet += 10
+            bet.config(text=f"Bet:{self.bet}")
+
+    def decrease_bet(self):
+        if self.bet > 10:
+            self.bet -= 10
+            bet.config(text=f"Bet:{self.bet}")
 
 slot = slot(1000)
 root = Tk()
@@ -65,7 +74,17 @@ output2.grid(column=1, row=1)
 output3 = ttk.Label(frm, text="7",font=("Arial", 30, "bold"), width=3, anchor='center')
 output3.grid(column=2, row=1)
 
+bet = ttk.Label(frm, text=f"Bet:{slot.bet}",font=("Arial", 30, "bold"), width=7, anchor='center')
+bet.grid(column=1, row=3)
+
 spinButton = ttk.Button(frm, text="SPIN",command=slot.spin)
 spinButton.grid(column=1, row=2)
+
+raiseBetButton = ttk.Button(frm, text="Raise Bet",command=slot.increase_bet)
+raiseBetButton.grid(column=2, row=3)
+
+lowerBetButton = ttk.Button(frm, text="Lower Bet",command=slot.decrease_bet)
+lowerBetButton.grid(column=0, row=3)
+
 
 root.mainloop()
